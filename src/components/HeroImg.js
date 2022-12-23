2748e5df813fb61a6b4997115444c42d8fb5a922
prashijain <prashijain1996@gmail.com>
2022-12-23 16:19:19 +0530

    Created title component and made it responsive

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,13 +1,25 @@
 import "./HeroImgStyles.css";
-
+import IntroImg from "../assets/intro-bg.jpeg";
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero"> 
     <div className="mask">
+<img src={IntroImg} alt="IntroImg" className="intro-img" />
 
-        
+    </div>
+    <div className="content">
+        <p>HI, I am Prashi Jain</p>
+        <h1>Full Stack Developer</h1>
+        <div>
+            <Link to ="/project"
+            className="btn">Projects</Link>
+               <Link to ="/contact"
+            className="btn btn-light">
+                Contact</Link>
+        </div>
     </div>
     </div>
   )
